fa7e6e35127a5ab5735cb99d8a524e4b6c916427
Adnan <skadnan947@gmail.com>
2026-09-11 19:11:21 +0530

    Added Comments

--- src/com/adnan/java8_9_11/Java_8/InterfaceChanges1/Main.java
+++ src/com/adnan/java8_9_11/Java_8/InterfaceChanges1/Main.java
@@ -34,7 +34,10 @@ public class Main implements Test {
 
 /*
 From Java 1.8 we can also write Default and Static methods inside an interface
+
 Default method use case:
+Default methods provide Backward compatibility means:
+
 For example if we have one interface and 3 implementation classes of that interface
 and there are 2 methods which we only want inside one class or lets say 2 classes among three
 3 implemented classes then we have to write abstract methods inside an interface and
@@ -49,6 +52,17 @@ We cannot override static variables or methods inside implementation because sta
 means for static variables/methods memory will be allocated only once in stack during program start.
 where as objects get stored in Heap memory. we will be using Static methods directly by using
 interfaceName.static method name
+
+UseCase of Static Method :
+we can avoild creating seperate classes for utility methods and can write utility methods
+inside the interface itself
+For Example:Before Java 1.8 Collection interface was introduced as there was no scope to
+write static/utility methods inside an interface creators created a seperate class called as
+Collections which hold all the utility ethods related to all the collections
+
+As we know ArrayList is not synchronized if we want to synchronize Arraylist we have to use
+utility method present in collections class called as .synchronizeList() as there was no scope
+to write these sort of methods in interface before java 1.8.
  */
 
 
--- src/com/adnan/java8_9_11/Java_8/Lambda2/SimpleLambda1/Main.java
+++ src/com/adnan/java8_9_11/Java_8/Lambda2/SimpleLambda1/Main.java
@@ -26,4 +26,21 @@ variablename.methodname() to call that method.
 But if we have a requirement lets say to create 10 functional interfaces which have only
 one abstract method then writing 10 interfaces will make our Jar file heavy this is the
 disadvantage to overcome this we have predefined Functional interfaces.
+
+Benefits of Lambda :
+1) Lambda enables Functional Programming means
+if any method is taking reference of an Functional interface example :
+public void method1(Test1);     here Test1 is an Functional interface
+now if we want to pass Test1 as argument before java 8 we have to create a class which is
+implementing Test1 interface then create object of that class then pass it to method1(object);
+
+But from java8 if passed method is Functional interface then instead of creating a seperate class
+we can directly write lambda in place of expression.
+Example:whenever we are creating Thread then thread class takes Runnable Interface as reference
+there we can directly write lambda
+
+Thread t = new Thread(()->sout("Hello"));
+
+2)By using Lambda we can replace anonymous classes same thread example.
+
  */
